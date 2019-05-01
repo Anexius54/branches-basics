8279748bd17e67337ad89e2092a931bb27b09c79
Evgeniy Sapronov <ek.sapronov@gmail.com>
2019-05-01 20:52:13 +0700

    Modificate "Hello, world!"

--- main.c
+++ main.c
@@ -2,6 +2,14 @@
 
 int main()
 {
+    int array[5];
+    for (size_t i = 0; i < 5; i++){
+	array[i] = i * i;
+    }
     printf("Hello, world!");
+    for (size_t i = 0; i < 5; i++){
+	printf("#%ld = %d\n", i, array[i]);
+    }
+    printf("\n");
     return 0;
 }
