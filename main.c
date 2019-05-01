a8321e217d63b61c0f5ffa553f75ef933806aeca
Evgeniy Sapronov <ek.sapronov@gmail.com>
2019-05-01 21:06:44 +0700

    Modificate program

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 
 int main()
 {
-    int array[5];
-    for (size_t i = 0; i < 5; i++){
+    int n = 5;
+    int array[n];
+    for (size_t i = 0; i < n; i++){
 	array[i] = i * i;
     }
     printf("Hello, world!");
-    for (size_t i = 0; i < 5; i++){
+    for (size_t i = 0; i < n; i++){
 	printf("#%ld = %d\n", i, array[i]);
     }
     printf("\n");
